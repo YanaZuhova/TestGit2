3ef0ae0e9bcd17e512b0014ea4ae697f22109e37
Iana <ziukhovaiana@gmail.com>
2024-05-10 10:31:39 +0300

    module1005

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import {arts, dataArt,} from ".module.js";
+
 let articles = document.getElementById("allArticles");
 articles.innerText = "Все статьи (3)";
 
@@ -24,41 +26,6 @@ function scrollToElement(element) {
   });
 
   let dynamicDiv = document.getElementById("dynamic");
-
-  let arts = [
-    { title: "Why JS", text: "Lorem ipsum dolor sit! Inventore?", file: "JS.html", linkText: "Читать статью о JS"},
-    { title: "Tехнологии", text: "Статья про технологии", file: "Tekhnolog.html", linkText: "Читать статью о Tekhno"},
-];
-
-  let dataArt = arts.map((art) => {
-  let { title, text, file, linkText } = art;
-
-  // console.log(title);
-  // console.log(text);
-  // console.log(file);
-
-  let artCard = document.createElement("div");
-    artCard.className = "element";
-
-    const titleElement = document.createElement("h3");
-    titleElement.innerText = title;
-    artCard.append(titleElement);
-
-    const textElement = document.createElement("p");
-    textElement.innerText = text;
-    artCard.append(textElement);
-
-    // const fileElement = document.createElement("a");
-    // fileElement.href = file;
-    // artCard.append(fileElement);
-
-    const linkTextElement = document.createElement("a");
-    linkTextElement.href = file;
-    linkTextElement.innerText = linkText;
-    artCard.append(linkTextElement);
-
-    return artCard;
-});
 
 console.log(dataArt);
 
---
+++ module.js
@@ -0,0 +1,36 @@
+export
+let arts = [
+    { title: "Why JS", text: "Lorem ipsum dolor sit! Inventore?", file: "JS.html", linkText: "Читать статью о JS"},
+    { title: "Tехнологии", text: "Статья про технологии", file: "Tekhnolog.html", linkText: "Читать статью о Tekhno"},
+];
+
+export
+  let dataArt = arts.map((art) => {
+  let { title, text, file, linkText } = art;
+
+  // console.log(title);
+  // console.log(text);
+  // console.log(file);
+
+  let artCard = document.createElement("div");
+    artCard.className = "element";
+
+    const titleElement = document.createElement("h3");
+    titleElement.innerText = title;
+    artCard.append(titleElement);
+
+    const textElement = document.createElement("p");
+    textElement.innerText = text;
+    artCard.append(textElement);
+
+    // const fileElement = document.createElement("a");
+    // fileElement.href = file;
+    // artCard.append(fileElement);
+
+    const linkTextElement = document.createElement("a");
+    linkTextElement.href = file;
+    linkTextElement.innerText = linkText;
+    artCard.append(linkTextElement);
+
+    return artCard;
+});
